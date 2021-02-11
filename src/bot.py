dc7cb180130290bc887a3720c247acef4f78e08a
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2021-02-11 12:11:04 -0300

    answering question by mention. bot aren't replying the same comment. all methods separated

--- src/bot.py
+++ src/bot.py
@@ -1,27 +1,57 @@
-from credentials.api_keys import APIKeys
+import praw
 
-subreddit = APIKeys.reddit.subreddit('TrainingBots')
-hot_posts = subreddit.hot(limit=5)
+reddit = praw.Reddit(
+    client_id='x',
+    client_secret='x',
+    username='x',
+    password='x',
+    user_agent='x',
+)
 
-already_answered = None
+subreddit = reddit.subreddit('TrainingBots')
+hot_posts = subreddit.hot(limit=None)
 
+
+# prefix (already lowercase)
+def asking_about(ask):
+    return "u/rocketinfo " + ask
+
+
+# collections of replies
+def answer_about(ask):
+    if ask == 'falcon 1':
+        print('Someone asked about Falcon 1! :D')
+
+        mention.reply('Falcon 1 information!')
+        mention.mark_read()
+        print("Answered it!")
+
+    if ask == 'falcon 9':
+        print('Someone asked about Falcon 9! :D')
+        # worked
+        mention.reply(
+            '| Cost per launch | $66, 000 |    \n' +
+            '| Cost of production | $100, 000 |    \n'
+        )
+        mention.mark_read()
+        print("Answered it!")
+
+
+post_number = 1
+# getting all subreddit posts
 for post in hot_posts:
     if not post.stickied:
 
-        # getting all comments
-        for comment in post.comments:
-            # someone mentioned a rocket!
-            if 'falcon 1' in comment.body:
-                for reply in comment.replies:
-                    if reply.body == 'Falcon 1 informations!':
-                        already_answered = True
-                    else:
-                        already_answered = False
-                if not already_answered:
-                    comment.reply('Falcon 1 informations!')
-                else:
-                    print('I already answered about Falcon 1.')
+        print("I'm going try to find someone needing help with the " +
+              str(post_number) + "º post(s).")
+        # getting all unread mentions
+        for mention in reddit.inbox.unread(limit=None):
+            user_request = str(mention.body).lower()
 
-                already_answered = False
+            if user_request == asking_about('falcon 1'):
+                answer_about('falcon 1')
+            if user_request == asking_about('falcon 9'):
+                answer_about('falcon 9')
+    post_number += 1
 
-print('bot shutdown.')
+print('That is! I am going to turn off... :/')
